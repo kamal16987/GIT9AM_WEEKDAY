5af58d677a748dd83159394c32ff3dc1b101f0d9
Deepika411 <deepikame13@gmail.com>
2019-03-18 19:10:30 +0530

    sum of even nos

---
+++ src/com/test/packa/Deepika_02.java
@@ -0,0 +1,14 @@
+package com.test.packa;
+
+public class Deepika_02 {
+public static void main(String[] args) {
+	int sum=0;
+	for (int i = 1; i<=10; i++) {
+		if(i%2==0)
+		{
+			sum=sum+i;
+		}
+	}
+	System.out.println(sum);
+}
+}
