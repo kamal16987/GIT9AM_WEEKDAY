baaff83244160d7f9638feaa7fe345d5e08743d8
vetrikrrish <vetrikrrish@gmail.com>
2019-03-19 11:58:38 +0530

    added vinoth file

---
+++ src/com/test/packa/Vinoth.java
@@ -0,0 +1,21 @@
+package com.test.packa;
+
+public class Vinoth {
+
+	public static void main(String[] args) {
+		int a=234, i=0, j=0;
+		while(a>0) {
+			i=a%10;
+			j=j*10+i;
+			a=a/10;
+		}
+		System.out.println(j);
+		}
+		
+	
+		
+
+
+	}
+
+
