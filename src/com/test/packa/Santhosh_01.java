3438427ee0127891a5f9b1bb90e063daa0d33294
Amzeth <amzeth1992@users.noreply.github.com>
2019-03-19 09:04:51 +0530

    Base class created

---
+++ src/com/test/packa/Santhosh_01.java
@@ -0,0 +1,10 @@
+package com.test.packa;
+
+public class Santhosh_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Base Class Created");
+	}
+
+}
